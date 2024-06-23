ba30056d9650ad477cd5a2107b11f14e5cff5bed
Breno Tebet <brenotebet@live.com>
2024-06-23 15:18:42 -0500

    Last Commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,9 +5,12 @@ const Header = () => {
   return (
     <div>
         <div className="header">
-            <div className="header-contents">
             <h2>Sua parada é aqui!</h2>
-
+            <div className="header-contents">
+            <div className="intro">
+            <p>Bem-vindo a City Stop! Aqui você encontra uma seleção especial de cervejas,
+              destilados, carnes congeladas, queijos, bebidas não-alcoólicas, massas e mercearia/conveniencia. Venha nos visitar! Na City Stop, a qualidade e a satisfação dos nossos clientes são nossas prioridades.</p>
+            </div>
             <div className="horarios">
               <p>horários de funcionamento:</p>
               <p>Segunda e terça: 10h às 20h</p>
